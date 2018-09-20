3064d2fc4e91738f0b3dcef67547cdc1264c07d9
Mark Dyehouse <markdyehouse2019@u.northwestern.edu>
2018-09-20 15:06:39 -0500

    Testing with only red filtering in orig file, new file is for circle tracking

--- src/imgProc.py
+++ src/imgProc.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python -tt
 
+import numpy as np
 import cv2
 
 a = cv2.VideoCapture(-1)
@@ -7,12 +8,22 @@ a = cv2.VideoCapture(-1)
 while(True):
     # Capture frame-by-frame
     ret, frame = a.read()
+    colorMinThresh = [0, 0, 150]
+    colorMaxThresh = [75, 75, 255]
 
     # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    red = cv2.inRange(frame, np.array(colorMinThresh), np.array(colorMaxThresh))
 
+    #d = cv2.SimpleBlobDetector_create()
+    #keypoints = d.detect(red)
+
+    #kp = cv2.drawKeypoints(red, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+    im2, contours, hierarchy = cv2.findContours(red, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    newim = cv2.drawContours(frame, contours, -1, (0,255,0), 3)
     # Display the resulting frame
-    cv2.imshow('frame',gray)
+    cv2.imshow('frame',newim)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
